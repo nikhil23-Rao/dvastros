9b491a05a289d032a5bcad5872e3e81290bf6b1a
Pratyush Mahadevaiah <pratyush.mahadevaiah@gmail.com>
2024-12-08 21:13:05 -0800

    added basic movement

--- DV Astros/Assets/PathCreator/Examples/Scripts/GeneratePathExample.cs
+++ DV Astros/Assets/PathCreator/Examples/Scripts/GeneratePathExample.cs
@@ -1,16 +1,52 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
 
 namespace PathCreation.Examples {
     // Example of creating a path at runtime from a set of points.
 
     [RequireComponent(typeof(PathCreator))]
+
+    
+
     public class GeneratePathExample : MonoBehaviour {
 
         public bool closedLoop = true;
-        public Transform[] waypoints;
+        public List<Transform> waypoints = new List<Transform>();
+
+        public GameObject program;
 
         void Start () {
-            if (waypoints.Length > 0) {
+
+            //waypoints = program.GetData
+
+            string[,] data = program.GetComponent<Program>().GetData();
+            for (int i = 0; i < data.Length; i++) {
+                float valueX = 0;
+                float valueY = 0;
+                float valueZ = 0;
+                try {
+                    valueX = float.Parse(data[i, 0]);
+                }
+                catch (Exception e) { Debug.Log("Catch"); }
+                try {
+                    valueY = float.Parse(data[i, 1]);
+                }
+                catch (Exception e) { Debug.Log("Catch"); }
+
+                try {
+                    valueZ = float.Parse(data[i, 2]);
+                }
+                catch (Exception e) { Debug.Log("Catch"); }
+
+                GameObject emptyGO = new GameObject();
+                Transform newTransform = emptyGO.transform;
+                newTransform.position = new Vector3(valueX, valueY, valueZ);
+                waypoints.Add(newTransform);
+            }
+
+            if (waypoints.Count > 0) {
                 // Create a new bezier path from the waypoints.
                 BezierPath bezierPath = new BezierPath (waypoints, closedLoop, PathSpace.xyz);
                 GetComponent<PathCreator> ().bezierPath = bezierPath;
